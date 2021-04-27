08d676a0807061bc2a11d6e09ca595c691e90c22
Pedro Gonçalves <pedro@unimestre.com>
2021-04-27 17:39:00 -0300

    Módulo 2 - Algoritmos -> Exercício 1

---
+++ exercicios/1-introducao/exercicio-0.php
@@ -0,0 +1,17 @@
+<?php 
+
+// Criando a classe "HelloWorld"
+class HelloWorld {
+
+    // Método "print()" da classe
+    function print() {
+        echo("Hello World");
+    }
+
+}
+
+// Criando uma instância da classe "HelloWorld"
+$objHelloWorld = new HelloWorld();
+
+// Chamando o método "print()"
+$objHelloWorld->print();
--- exercicios/1-introducao/exercicio-1.php
+++ exercicios/1-introducao/exercicio-1.php
@@ -1,21 +1,24 @@
 <?php 
 
 // Criando a classe "HelloWorld"
-// Creating the "HelloWorld" class
 class HelloWorld {
 
     // Método "print()" da classe
-    // Class method "print()"
     function print() {
+        echo("Hello World");
+    }
+
+    // Método "printTwoLines()" da classe
+    function printTwoLines() {
+        echo("Hello World");
+        echo("<br />");
         echo("Hello World");
     }
 
 }
 
 // Criando uma instância da classe "HelloWorld"
-// Creating an instance of the "HelloWorld" class
 $objHelloWorld = new HelloWorld();
 
-// Chamando o método "print()"
-// Calling the "print()" method
-$objHelloWorld->print();
+// Chamando o método "printTwoLines()"
+$objHelloWorld->printTwoLines();
