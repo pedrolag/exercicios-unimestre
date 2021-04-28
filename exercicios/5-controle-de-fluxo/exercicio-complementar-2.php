1bc7b928fe4e2719df60bc42501a970a4f35034a
Pedro Gonçalves <pedro@unimestre.com>
2021-04-28 13:14:25 -0300

    Módulo 5 - Controle de Fluxo - Exercicio Complementar 2

---
+++ exercicios/5-controle-de-fluxo/exercicio-complementar-2.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Pág 45 - Exercícios Complementares - Número 2
+ */
+
+for ($x = 1; $x <= 10; $x ++) {
+    for ($y = 1; $y <= 10; $y ++) {
+        $result = $x * $y;
+        echo("$x x $y = $result <br />");
+    }
+    echo("<br />");
+}
