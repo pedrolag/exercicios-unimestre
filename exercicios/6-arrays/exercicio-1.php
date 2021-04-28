b6f8af6789fd2607e8683ce5af326a9176739266
Pedro Gonçalves <pedro@unimestre.com>
2021-04-28 16:57:57 -0300

    Módulo 6 - Arrays -> Exercício 1

---
+++ exercicios/6-arrays/exercicio-1.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Pág 55 - Exercícios de Fixação - Número 2
+ */ 
+
+$arrNumeros = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10
+];
+
+for ($x = 0; $x < count($arrNumeros); $x ++) {
+    echo("$arrNumeros[$x] <br />");
+}
