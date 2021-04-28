5a27b1b4078731f6fbf17f2e6cd3f7c8d29af9ef
Pedro Gonçalves <pedro@unimestre.com>
2021-04-28 10:22:19 -0300

    Módulo 5 - Controle de Fluxo -> Exercício 3

---
+++ exercicios/5-controle-de-fluxo/exercicio-3.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Classe Saudacao
+ */
+class Saudacao 
+{
+    /** @var int $nr_hora hora atual */
+    public $nr_hora = 22;
+}
+
+// Criando uma instância da classe "Saudacao"
+$objSaudacao = new Saudacao();
+
+if ($objSaudacao->nr_hora >= 0 && $objSaudacao->nr_hora < 12) {
+
+    echo("Bom dia");
+
+} else if ($objSaudacao->nr_hora >= 12 && $objSaudacao->nr_hora < 18) {
+
+    echo("Boa tarde");
+
+} else if ($objSaudacao->nr_hora >= 18 && $objSaudacao->nr_hora < 24) {
+
+    echo("Boa noite");
+
+} else {
+
+    echo("Unibanco 30 horas :P");
+
+}
