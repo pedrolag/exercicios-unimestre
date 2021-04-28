c2e2abad160245b88e3fb65945825aefafeacb65
Pedro Gonçalves <pedro@unimestre.com>
2021-04-28 09:42:29 -0300

    Módulo 5 - Controle de Fluxo -> Exercício 1

---
+++ exercicios/5-controle-de-fluxo/exercicio-1.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Classe PrecoProduto
+ */
+class PrecoProduto 
+{
+    /** @var float $precoDoProduto1 preço do produto 1 */
+    public $precoDoProduto1 = 5325.12;
+
+    /** @var float $precoDoProduto2 preço do produto 2 */
+    public $precoDoProduto2 = 4366.34;
+}
+
+// Criando uma instância da classe "PrecoProduto"
+$objPrecoProduto = new PrecoProduto();
+
+if ($objPrecoProduto->precoDoProduto1 < $objPrecoProduto->precoDoProduto2) {
+
+    echo("O produto 2 é o mais caro.");
+
+} else if ($objPrecoProduto->precoDoProduto2 < $objPrecoProduto->precoDoProduto1) {
+
+    echo("O produto 1 é o mais caro.");
+
+}
