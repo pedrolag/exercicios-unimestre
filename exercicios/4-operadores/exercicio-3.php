ccb02fa294805ff38c57f341b150f4f026f5a4a3
Pedro Gonçalves <pedro@unimestre.com>
2021-04-28 09:02:47 -0300

    Módulo 4 - Operadores -> Exercício 3

---
+++ exercicios/4-operadores/exercicio-3.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Classe TestaOperadoresRelacionais
+ */
+class TestaOperadoresRelacionais 
+{
+    /** @var int $varA valor inicial */
+    public $varA = 1;
+
+    /** @var int $varB valor inicial */
+    public $varB = 2;
+}
+
+// Criando uma instância da classe "TestaOperadoresRelacionais"
+$objTestaOperadoresRelacionais = new TestaOperadoresRelacionais();
+
+// A > B
+$res = $objTestaOperadoresRelacionais->varA > $objTestaOperadoresRelacionais->varB;
+var_dump($res);  // Output: false
+echo("<br />");
+
+// A >= B
+$res = $objTestaOperadoresRelacionais->varA >= $objTestaOperadoresRelacionais->varB;
+var_dump($res);  // Output: false
+echo("<br />");
+
+// A < B
+$res = $objTestaOperadoresRelacionais->varA < $objTestaOperadoresRelacionais->varB;
+var_dump($res);  // Output: true
+echo("<br />");
+
+// A <= B
+$res = $objTestaOperadoresRelacionais->varA <= $objTestaOperadoresRelacionais->varB;
+var_dump($res);  // Output: true
+echo("<br />");
+
+// A == B
+$res = $objTestaOperadoresRelacionais->varA == $objTestaOperadoresRelacionais->varB;
+var_dump($res);  // Output: false
+echo("<br />");
+
+// A != B
+$res = $objTestaOperadoresRelacionais->varA != $objTestaOperadoresRelacionais->varB;
+var_dump($res);  // Output: true
+echo("<br />");
